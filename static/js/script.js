04ad54b67460c3cdef55b710625af1e2573f27de
Arjun AJ <aj110arjun@gmail.com>
2026-01-10 04:12:58 +0530

    update: variant price update using js

--- static/js/script.js
+++ static/js/script.js
@@ -3,11 +3,21 @@
 document.addEventListener('DOMContentLoaded', function(){
     const radios = document.querySelectorAll('input[name="payment_method"]');
     const button = document.getElementById('actionbtn');
-    const baseText = button.textContent.trim()
+    const baseText = button.textContent.trim();
+
+    const variants = document.querySelectorAll('input[name="variant"]')
+    const price = document.getElementById('price')
 
     radios.forEach(radio => {
         radio.addEventListener('change', function(){
             button.textContent = `${baseText} using ${this.value}`;
         });
     });
+
+    variants.forEach(variant => {
+        variant.addEventListener('change', function(){
+            price.textContent = this.dataset.price;
+        });
+    });
+
 });
